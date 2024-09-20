aed66752cfad3ca271519ebba372ee09bcb817cb
jamalsk9692 <139873056+jamalsk9692@users.noreply.github.com>
2024-09-20 09:53:52 +0530

    Update temp-otp-api.js

--- api/temp-otp-api.js
+++ api/temp-otp-api.js
@@ -1,32 +1,37 @@
-// pages/api/temp-mail-messages.js
+// pages/api/temp-mail-otp.js
 
 export default async function handler(req, res) {
+  // Get the token from the query string
+  const { token } = req.query;
 
-  // Get the token from the request body
-  const { token } = req.body;
+  // Check if the token is provided
+  if (!token) {
+    return res.status(400).json({ message: 'Token is required' });
+  }
 
   // Generate a random IP address
   const randomIP = `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`;
 
   try {
-    // Make a GET request using Fetch API
+    console.log(`Received token: ${token}`);
+
+    // Make a GET request to the third-party API
     const response = await fetch('https://mob2.temp-mail.org/messages', {
       method: 'GET',
       headers: {
         'User-Agent': '3.48',
         'Accept': 'application/json',
-        'authorization': token,  // Use the token from the request body
-        'X-Forwarded-For': randomIP,  // Include the random IP address in the header
+        'authorization': token,
+        'X-Forwarded-For': randomIP,
       },
     });
 
-    // Parse the response to JSON
+    // Parse the response as JSON
     const data = await response.json();
 
-    // Send the response back as JSON
+    // Send the JSON data back to the client
     res.status(200).json(data);
-  } catch {
-    // Handle errors and send an error message as the response
+  } catch (error) {
     res.status(500).json(data);
   }
 }
